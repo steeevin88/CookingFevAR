98ef1cf3ec1d5fa87d1285f4e7452d0e97c7c7ce
Steven Le <stevenleusa79@yahoo.com>
2023-11-12 06:57:04 -0800

    added missing script changes

--- Assets/CustomScripts/DuplicateObject.cs
+++ Assets/CustomScripts/DuplicateObject.cs
@@ -7,7 +7,7 @@ public class DuplicateObjectOnClick : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && GetComponent<Renderer>().material.color != Color.red)
+        if (Input.GetMouseButtonDown(0))
         {
             DuplicateObject();
         }
