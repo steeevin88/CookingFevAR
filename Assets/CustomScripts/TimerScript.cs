849d17ea5817764796a35b9b33ca3d76e9a8fba0
Steven Le <stevenleusa79@yahoo.com>
2023-11-11 22:02:08 -0800

    working timer

--- Assets/CustomScripts/TimerScript.cs
+++ Assets/CustomScripts/TimerScript.cs
@@ -1,45 +1,83 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class TimerScript : MonoBehaviour
+public class Timer : MonoBehaviour
 {
-    public float totalTime; // Set the total time for the timer in seconds
-    private float currentTime;
-    public Text timerText;
+    public float totalTime = 60f;  // Total time for the timer in seconds
+    public string item = "";
+    private float currentTime;      // Current time remaining
+    private bool isRunning = false; // Flag to check if the timer is currently running
+    private bool colorInterval = false;
 
-    public TimerScript(float startingTime){
-        this.totalTime = startingTime;
-    }
+    public TextMeshProUGUI timerText;  // Reference to the Text component for displaying the timer
 
     void Start()
     {
-        currentTime = totalTime;
+        ResetTimer();
+        isRunning = true;
     }
 
     void Update()
     {
-        if (currentTime > 0)
+        if (colorInterval && !isRunning)
         {
-            currentTime -= Time.deltaTime;
-            // Update your timer UI or perform actions based on time here
-            UpdateTimerUI();
-
+            timerText.color = Color.red;
+            colorInterval = !colorInterval;
+        } else if(!isRunning) { 
+            timerText.color = Color.white;
+            colorInterval = !colorInterval;
         }
-        else
-        {
-            // Timer has reached zero, perform actions when the timer is complete
-            Debug.Log("Timer complete!");
-        }
+        currentTime -= Time.deltaTime;  // Update the timer based on real-time
+        UpdateTimerText();
     }
 
-    void UpdateTimerUI()
+    // Method to start the timer
+    public void StartTimer()
     {
-        // Display the remaining time in the UI
-        if (timerText != null)
+        isRunning = true;
+    }
+
+    // Method to add 30 seconds to the timer
+    public void Add30Seconds()
+    {
+        currentTime += 30f;
+        UpdateTimerText();  // Update the timer text after adding time
+    }
+
+    // Method to reset the timer to the initial total time
+    public void ResetTimer()
+    {
+        currentTime = totalTime;
+        isRunning = false;
+        UpdateTimerText();  // Update the timer text after resetting
+    }
+
+    // Method to update the timer text
+    private void UpdateTimerText()
+    {
+        float currentTimeInSeconds = Mathf.Floor(currentTime);
+        if (currentTimeInSeconds % 2 == 0)
         {
-            timerText.text = "Time: " + Mathf.Round(currentTime);
+            // Set the text color to white if the time is even
+            timerText.color = Color.white;
         }
+        else if(currentTimeInSeconds < 0)
+        {
+            // Set the text color to red if the time is odd
+            timerText.color = Color.red;
+        }
+        if(currentTime < 0)
+        {
+            timerText.text = "00:00 - " + item;
+        }
+         else
+        {
+            int minutes = Mathf.FloorToInt(currentTime / 60);
+            int seconds = Mathf.FloorToInt(currentTime % 60);
+            string formattedTime = string.Format("{0:00}:{1:00}", minutes, seconds);
+            timerText.text = "" + formattedTime + " - " + item;
+        }
+
     }
 }
